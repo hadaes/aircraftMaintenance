23993b1c5e6f3e06ba54f064256e9e33f9ad26b7
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-07-20 19:18:57 -0500

    Added basic layout to home screen

--- lib/home.dart
+++ lib/home.dart
@@ -13,11 +13,42 @@ class Home extends StatelessWidget {
         ),
         backgroundColor: Colors.blue[900],
       ),
-      body: Container(
-        child: Center(
-          child: Text(
-            "Hi",
-          ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 80, 30, 0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircularProgressIndicator(
+                  backgroundColor: Colors.black,
+                ),
+                CircularProgressIndicator(
+                  backgroundColor: Colors.black,
+                ),
+                CircularProgressIndicator(
+                  backgroundColor: Colors.black,
+                )
+              ],
+            ),
+            Divider(
+              height: 100.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircularProgressIndicator(
+                  backgroundColor: Colors.black,
+                ),
+                CircularProgressIndicator(
+                  backgroundColor: Colors.black,
+                ),
+                CircularProgressIndicator(
+                  backgroundColor: Colors.black,
+                )
+              ],
+            ),
+          ],
         ),
       ),
       backgroundColor: Colors.blue[900],
