611876183cbc042f35d74592805a97e89d782697
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-07-24 21:27:31 -0500

    Locked orientation to potrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import "package:flutter/material.dart";
 import "package:aircraftMaintenance/home.dart";
+import "package:flutter/services.dart";
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       home: Home(),
     );
