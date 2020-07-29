2957c9deada99ce665e9e62ea45b0921d1dfe25d
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-07-28 20:45:19 -0500

    Added appbar, drawer, scaffold

--- lib/about.dart
+++ lib/about.dart
@@ -1,8 +1,16 @@
+import 'package:aircraftMaintenance/widgets/drawer_widget.dart';
 import "package:flutter/material.dart";
 
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("About"),
+      ),
+      drawer: DrawerWidget(),
+    );
   }
 }
