eea5a2a1a949e9348fe64dcf0c9cffce7c1819bc
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-07-28 21:00:42 -0500

    Added home button to drawer

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class DrawerWidget extends StatelessWidget {
-  const DrawerWidget({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,25 +12,48 @@ class DrawerWidget extends StatelessWidget {
                 color: Theme.of(context).primaryColor,
                 height: MediaQuery.of(context).size.height,
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: FlatButton(
-                    color: Colors.black,
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/about");
-                    },
-                    child: Text(
-                      "About",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 5, 5, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: FlatButton(
+                        color: Colors.black,
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/");
+                        },
+                        child: Text(
+                          "Home",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: FlatButton(
+                        color: Colors.black,
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/about");
+                        },
+                        child: Text(
+                          "About",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           )
         ],
