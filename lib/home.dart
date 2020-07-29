466f3e1485f66358415c838c6316d002a3682806
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-07-28 19:48:52 -0500

    Added functionality to drawer

--- lib/home.dart
+++ lib/home.dart
@@ -19,8 +19,33 @@ class Home extends StatelessWidget {
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
-            Text("About"),
-            Text("Home"),
+            Stack(
+              children: [
+                Container(
+                  color: Theme.of(context).primaryColor,
+                  height: MediaQuery.of(context).size.height,
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      color: Colors.black,
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/about");
+                      },
+                      child: Text(
+                        "About",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
