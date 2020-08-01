e249a3e5a6038f009b0c470da513236b1fa3779b
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-08-01 16:36:21 -0500

    Changed saveToFile method to correctly overwrite

--- lib/classes/file_utilis.dart
+++ lib/classes/file_utilis.dart
@@ -16,12 +16,40 @@ class FileUtilis {
 
   static Future<File> saveToFile(Map<String, dynamic> data) async {
     final file = await getFile;
-    try {
-      String dataString = jsonEncode(data);
-      return file.writeAsString(dataString);
-    } catch (e) {
-      print("Problem saving");
+    Map<String, dynamic> contents = await readFromFile();
+    // Three cases to consider when writing to file.
+    // 1: Nothing initalized so just write input map to file directly
+    // 2: If a item tracker has writen to the file, the previous data needs
+    //    to be overwritten with the new data
+    // 3: If the file has data in it but not relating to the item tracker we
+    //    are currently dealing with. Add a new section for it.
+    if (contents == null) {
+      try {
+        String dataString = jsonEncode(data);
+        return file.writeAsString(dataString);
+      } catch (e) {
+        print("Problem saving to non-initalized file");
+      }
     }
+    // else if (contents != null && contents.containsKey(data.keys)) {
+    //   try {
+    //     contents["${data.keys}"] = data[data.keys];
+    //     String jsonString = jsonEncode(contents);
+    //     return file.writeAsString(jsonString);
+    //   } catch (e) {
+    //     print("Problem overwriting contents");
+    //   }
+    // }
+    else {
+      try {
+        contents.addAll(data);
+        String contentString = jsonEncode(contents);
+        return file.writeAsString(contentString);
+      } catch (e) {
+        print("Problem saving to initialized file");
+      }
+    }
+
     return file;
   }
 
@@ -33,6 +61,7 @@ class FileUtilis {
       return jsonMap;
     } catch (e) {
       print("Error loading contents");
+      print(e);
     }
   }
 }
