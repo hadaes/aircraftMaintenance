6508de6cd8317e173c632553e99c6c9eed70d443
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-08-07 10:19:38 -0500

    Added autosize texct

--- lib/widgets/item_tracker.dart
+++ lib/widgets/item_tracker.dart
@@ -1,4 +1,5 @@
 import 'package:aircraftMaintenance/classes/file_utilis.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import "package:flutter/material.dart";
 import "dart:async";
 
@@ -57,18 +58,18 @@ class _ItemTrackerState extends State<ItemTracker> {
       width: MediaQuery.of(context).size.width * 0.25,
       child: Column(
         children: <Widget>[
-          Text(
+          AutoSizeText(
             widget.item,
             style: TextStyle(
                 color: Colors.white,
-                fontSize: 14.0,
                 fontWeight: FontWeight.w600,
                 fontFamily: "Montserrat"),
             maxLines: 1,
+            presetFontSizes: [18.0, 16.0, 14.0, 12.0],
           ),
           RaisedButton(
             color: Colors.black,
-            child: Text(
+            child: AutoSizeText(
               "Set date",
               style: TextStyle(
                 color: Colors.white,
@@ -77,20 +78,22 @@ class _ItemTrackerState extends State<ItemTracker> {
                 fontWeight: FontWeight.w800,
               ),
               textAlign: TextAlign.center,
+              maxLines: 1,
             ),
             onPressed: () {
               selectDate(context);
             },
           ),
-          Text(
+          AutoSizeText(
             "Due :  ${_date.month} - ${_date.day} - ${_date.year}",
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.w900,
               fontFamily: "Montserrat",
             ),
-            maxLines: 2,
-            overflow: TextOverflow.fade,
+            maxLines: 1,
+            presetFontSizes: [10.0, 9.0, 8.0, 7.0],
+            textAlign: TextAlign.center,
           ),
         ],
       ),
